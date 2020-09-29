d492734aef27f4e595546a61efc7b8e10de01ce8
mike32n <mihaly32horvath@gmail.com>
2020-09-29 23:59:16 +0200

    alternative login / no password login

--- src/test/java/com/codecool/jira/Login.java
+++ src/test/java/com/codecool/jira/Login.java
@@ -13,7 +13,6 @@ public class Login {
 
     @BeforeSuite
     public static void main(String[] args) {
-        // ChromeDriver location set up in Utils class
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
     }
 
@@ -26,6 +25,37 @@ public class Login {
         Thread.sleep(1000);
         driver.get("https://jira.codecool.codecanvas.hu/secure/ViewProfile.jspa");
         Assert.assertEquals(driver.findElement(By.id("up-d-username")).getText(), "user6");
+        driver.findElement(By.xpath("//a[@id='header-details-user-fullname']/span/span/img")).click();
+        driver.findElement(By.id("log_out")).click();
+    }
+
+    @Test(testName = "Alternative Login")
+    public static void alternativeLogin() throws InterruptedException {
+        driver.get("https://jira.codecool.codecanvas.hu/login.jsp");
+        driver.findElement(By.id("login-form-username")).sendKeys("user6");
+        driver.findElement(By.id("login-form-password")).sendKeys("CoolCanvas19.");
+        driver.findElement(By.id("login-form-submit")).click();
+        Thread.sleep(1000);
+        driver.get("https://jira.codecool.codecanvas.hu/secure/ViewProfile.jspa");
+        Assert.assertEquals(driver.findElement(By.id("up-d-username")).getText(), "user6");
+        driver.findElement(By.xpath("//a[@id='header-details-user-fullname']/span/span/img")).click();
+        driver.findElement(By.id("log_out")).click();
+    }
+
+    @Test(testName = "No Password Login")
+    public static void noPasswordLogin() throws InterruptedException {
+        driver.get("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
+        driver.findElement(By.id("login-form-username")).sendKeys("user6");
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(1000);
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='usernameerror']/p")).getText(), "Sorry, your username and password are incorrect - please try again.");
+        driver.get("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
+        driver.findElement(By.id("login-form-username")).sendKeys("user6");
+        driver.findElement(By.id("login-form-password")).sendKeys("CoolCanvas19.");
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//a[@id='header-details-user-fullname']/span/span/img")).click();
+        driver.findElement(By.id("log_out")).click();
     }
 
     @AfterSuite
