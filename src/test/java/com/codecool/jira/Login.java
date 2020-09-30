3dbd7297b688444f26ddd73f264f796561eee2f9
mike32n <mihaly32horvath@gmail.com>
2020-09-30 10:29:27 +0200

    login without credentials

--- src/test/java/com/codecool/jira/Login.java
+++ src/test/java/com/codecool/jira/Login.java
@@ -26,7 +26,7 @@ public class Login {
         driver.findElement(By.id("login")).click();
         Thread.sleep(1000);
         driver.get("https://jira.codecool.codecanvas.hu/secure/ViewProfile.jspa");
-        Assert.assertEquals(driver.findElement(By.id("up-d-username")).getText(), "user6");
+        Assert.assertEquals(driver.findElement(By.id("up-d-username")).getText(), ReadLoginProperties.getUsername());
         driver.findElement(By.xpath("//a[@id='header-details-user-fullname']/span/span/img")).click();
         driver.findElement(By.id("log_out")).click();
     }
@@ -39,15 +39,30 @@ public class Login {
         driver.findElement(By.id("login-form-submit")).click();
         Thread.sleep(1000);
         driver.get("https://jira.codecool.codecanvas.hu/secure/ViewProfile.jspa");
-        Assert.assertEquals(driver.findElement(By.id("up-d-username")).getText(), "user6");
+        Assert.assertEquals(driver.findElement(By.id("up-d-username")).getText(), ReadLoginProperties.getUsername());
         driver.findElement(By.xpath("//a[@id='header-details-user-fullname']/span/span/img")).click();
         driver.findElement(By.id("log_out")).click();
     }
 
-    @Test(testName = "No Password Login")
-    public static void noPasswordLogin() throws InterruptedException, IOException {
+    @Test(testName = "Login Without Password")
+    public static void loginWithoutPassword() throws InterruptedException, IOException {
         driver.get("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
         driver.findElement(By.id("login-form-username")).sendKeys(ReadLoginProperties.getUsername());
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(1000);
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='usernameerror']/p")).getText(), "Sorry, your username and password are incorrect - please try again.");
+        driver.get("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
+        driver.findElement(By.id("login-form-username")).sendKeys(ReadLoginProperties.getUsername());
+        driver.findElement(By.id("login-form-password")).sendKeys(ReadLoginProperties.getPassword());
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//a[@id='header-details-user-fullname']/span/span/img")).click();
+        driver.findElement(By.id("log_out")).click();
+    }
+
+    @Test(testName = "Login Without Credentials")
+    public static void loginWithoutCredentials() throws InterruptedException, IOException {
+        driver.get("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
         driver.findElement(By.id("login")).click();
         Thread.sleep(1000);
         Assert.assertEquals(driver.findElement(By.xpath("//div[@id='usernameerror']/p")).getText(), "Sorry, your username and password are incorrect - please try again.");
