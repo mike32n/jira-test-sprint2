ed7a892c19630b4729d8274b909748e8ece75295
mike32n <mihaly32horvath@gmail.com>
2020-10-01 22:33:46 +0200

    glass versions filled optional fields

--- src/test/java/com/codecool/jira/GlassVersions.java
+++ src/test/java/com/codecool/jira/GlassVersions.java
@@ -59,6 +59,51 @@ public class GlassVersions {
         driver.findElement(By.id("log_out")).click();
     }
 
+    @Test(testName = "New Project Version In Glass")
+    public static void newProjectVersionInGlass() throws InterruptedException, IOException {
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.get("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
+        Thread.sleep(500);
+        driver.findElement(By.id("login-form-username")).sendKeys(ReadLoginProperties.getUsername());
+        driver.findElement(By.id("login-form-password")).sendKeys(ReadLoginProperties.getPassword());
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(1000);
+
+        driver.get("https://jira.codecool.codecanvas.hu/projects/PP1?selectedItem=com.codecanvas.glass:glass");
+        Thread.sleep(1500);
+
+        driver.findElement(By.xpath("//section[@id='content']/div/div/div/nav/div/div[2]/ul/li[3]/a")).click();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//form[@id='releases-add__version']/div/input")).sendKeys("Test PP1");
+        driver.findElement(By.xpath("//form[@id='releases-add__version']/div[2]/input")).sendKeys("1/oct/20");
+        driver.findElement(By.xpath("//form[@id='releases-add__version']/div[3]/input")).sendKeys("31/oct/20");
+        driver.findElement(By.xpath("//form[@id='releases-add__version']/div[4]/input")).sendKeys("Test Description");
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//form[@id='releases-add__version']/div[5]/button")).click();
+        Thread.sleep(500);
+        driver.findElement(By.linkText("Test PP1")).click();
+        Thread.sleep(1000);
+        driver.get("https://jira.codecool.codecanvas.hu/projects/PP1?selectedItem=com.codecanvas.glass:glass");
+        Thread.sleep(1500);
+        driver.findElement(By.id("aui-uid-2")).click();
+        Thread.sleep(1500);
+        Assert.assertTrue(driver.getPageSource().contains(driver.findElement(By.linkText("Test PP1")).getText()));
+
+        driver.findElement(By.xpath("//section[@id='content']/div/div/div/nav/div/div[2]/ul/li[3]/a")).click();
+        Thread.sleep(500);
+        driver.findElement(By.linkText("Operations")).click();
+        Thread.sleep(500);
+        driver.findElement(By.linkText("Delete")).click();
+        Thread.sleep(500);
+        driver.findElement(By.id("submit")).click();
+        Thread.sleep(500);
+
+        driver.findElement(By.xpath("//a[@id='header-details-user-fullname']/span/span/img")).click();
+        driver.findElement(By.id("log_out")).click();
+    }
+
+
     @AfterSuite
     public static void cleanup() {
         driver.manage().deleteAllCookies();
